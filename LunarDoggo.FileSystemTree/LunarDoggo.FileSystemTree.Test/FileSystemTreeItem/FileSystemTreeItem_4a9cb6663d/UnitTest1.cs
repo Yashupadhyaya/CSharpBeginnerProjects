17830a3c9bb29db22adf17cc73f28313bded78e4
Yashupadhyaya <50600305+Yashupadhyaya@users.noreply.github.com>
2023-07-30 20:10:19 +0530

    Test generated by RoostGPT for test unt-test using AI Type Azure Open AI and AI Model roost-gpt

---
+++ LunarDoggo.FileSystemTree/LunarDoggo.FileSystemTree.Test/FileSystemTreeItem/FileSystemTreeItem_4a9cb6663d/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace FileSystemTreeItem_4a9cb6663d;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
